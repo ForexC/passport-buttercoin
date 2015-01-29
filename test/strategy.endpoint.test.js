728cd1d7c3dca49f0c92faa8068e5f75cf6342a9
Bennett Hoffman <bennett@buttercoin.com>
2015-01-28 16:49:05 -0800

    Automatically coerce string to Endpoint when creating a strategy

--- test/strategy.endpoint.test.js
+++ test/strategy.endpoint.test.js
@@ -20,6 +20,24 @@ describe('Strategy#Endpoint', function() {
     expect(dev.endpoint).to.equal(endpoint);
     expect(dev.authorizationURL).to.equal(endpoint + '/auth');
     expect(dev.tokenURL).to.equal(endpoint + '/token');
+
+    var strategy = new ButtercoinStrategy({
+      clientID: 'fake',
+      clientSecret: 'fake',
+      endpoint: dev
+    }, function() {});
+
+    expect(strategy._oauth2._authorizeUrl).to.equal(endpoint + '/auth');
+    expect(strategy._oauth2._accessTokenUrl).to.equal(endpoint + '/token');
+
+    strategy = new ButtercoinStrategy({
+      clientID: 'fake',
+      clientSecret: 'fake',
+      endpoint: 'http://localhost:8080/OA2'
+    }, function() {});
+
+    expect(strategy._oauth2._authorizeUrl).to.equal(endpoint + '/auth');
+    expect(strategy._oauth2._accessTokenUrl).to.equal(endpoint + '/token');
   });
 
   it('should default to sandbox if no endpoint is specified', function() {
